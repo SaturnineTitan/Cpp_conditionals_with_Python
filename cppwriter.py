172850f20fd52432b8174bf73d37583c0bb601c5
Juno of Jupiter <43506369+SaturnineTitan@users.noreply.github.com>
2023-07-25 21:46:01 -0400

    Update cppwriter.py
    Formatted for readability

--- cppwriter.py
+++ cppwriter.py
@@ -1,5 +1,9 @@
-strings = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine"} # dictionary with values in question 
-f = open("output.cpp", "w") # open the output file in write mode 
-f.write('if (n > 9) {' + '\n\t' + 'printf("Greater than 9");' + '\n' + '}\n') # set up the first conditional check 
-for item in strings.keys(): # write the rest of the conditionals with a loop 
+# dictionary with values in question 
+strings = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine"}
+# open the output file in write mode 
+f = open("output.cpp", "w")
+ # set up the first conditional check 
+f.write('if (n > 9) {' + '\n\t' + 'printf("Greater than 9");' + '\n' + '}\n')
+# write the rest of the conditionals with a loop 
+for item in strings.keys():
 	f.write('else if (n == ' + str(item) + ')' + ' {' + '\n\t' + 'printf("' + strings[item] + '");' + '\n' + '}' + '\n')
